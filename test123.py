d77c70109a417f500e45b3e784d0922b67929b06
조형준 <gudwns9009@naver.com>
2025-07-22 11:50:07 +0900

    22일도 파이팅

--- test123.py
+++ test123.py
@@ -1,11 +1,3 @@
-a,b=map(int, input().split())
-arr = [0]*6
+a,b = input().split()
 
-for i in range(6):
-    arr[i] = a
-    a += 1
-
-    if(arr[i] > b):
-        break
-    
-    print(arr[i], end='')
+print(ord(b) - ord(a))
